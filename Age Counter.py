97d97f172f9ca081ba239c584a00cfd12841fe77
Afnan <alian777alian777a1@gmail.com>
2024-09-04 16:54:43 +0600

    first commit

---
+++ Age Counter.py
@@ -0,0 +1,18 @@
+def enterage(age):
+
+    if age < 0:
+        raise ValueError("Onlypositive integers are are allowed")
+    if age % 2 == 0:
+        print("age is even")
+    else:
+        print("age is odd")
+
+try:
+    num = int(input("Enter your age:"))
+    enterage(num)
+
+except ValueError:
+    print("only positive integers are allowed")
+
+except:
+    print("something is wrong")
